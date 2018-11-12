924f77dfa85e2bf7df04118d6aacb951aed87a99
Marrucha <mmatczuk@g.pl>
2018-11-12 20:48:55 +0100

    Calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,16 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
 
+        Calculator kalk = new Calculator(1,2);
+        if (kalk.add()==3)
+            System.out.println("Calculator.add() function is ok");
+        if (kalk.substract()==-1)
+            System.out.println("Calculator.substract() function is ok");
     }
 }
