f322d00f3da6b992d9af898a07d93dccdfe06234
Marrucha <mmatczuk@g.pl>
2018-11-14 20:47:40 +0100

    Zadanie z modułu 6.4

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumComment.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.forum;
+
+import java.util.*;
+
+public class ForumComment {
+    ForumPost forumPost;
+    String commentBody;
+    String author;
+
+    public ForumComment(ForumPost forumPost, String commentBody, String author) {
+        this.forumPost = forumPost;
+        this.commentBody = commentBody;
+        this.author = author;
+    }
+
+    public ForumPost getForumPost() {
+        return forumPost;
+    }
+
+    public void setForumPost(ForumPost forumPost) {
+        this.forumPost = forumPost;
+    }
+
+    public String getCommentBody() {
+        return commentBody;
+    }
+
+    public void setCommentBody(String commentBody) {
+        this.commentBody = commentBody;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ForumComment)) return false;
+        ForumComment that = (ForumComment) o;
+        return Objects.equals(getForumPost(), that.getForumPost()) &&
+                Objects.equals(getCommentBody(), that.getCommentBody()) &&
+                Objects.equals(getAuthor(), that.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getForumPost(), getCommentBody(), getAuthor());
+    }
+}
