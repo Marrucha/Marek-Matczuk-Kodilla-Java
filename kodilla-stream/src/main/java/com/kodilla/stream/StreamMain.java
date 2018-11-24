3e3ccccdf7f5fc814b754433de7fb93680b21060
Marrucha <mmatczuk@g.pl>
2018-11-24 18:00:36 +0100

    7.6 Average distance

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,37 +1,23 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.forumuser.Forum;
-import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.*;
-import java.time.temporal.*;
-import java.util.*;
-import java.util.stream.*;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 
 public class StreamMain {
     public static void main(String[] args) {
         LocalDate nowDate = LocalDate.now();
+        LocalDate other = LocalDate.now().minusDays(20);
+        long days = DAYS.between(other,nowDate);
 
-        Forum forum = new Forum();
-
-        forum.getUserList().stream()
-                .forEach(System.out::println);
-
-        Map<Integer,ForumUser> myList = forum.getUserList().stream()
-                    .filter(s-> s.getSex()== 'M')
-                    .filter(s-> ChronoUnit.YEARS.between( s.getBirthDate(),nowDate)>=20)
-                    .filter(s-> s.getPosts()>0)
-                    .collect(Collectors.toMap(ForumUser::getId,Forum->Forum));
+        System.out.println(days);
 
 
-        System.out.println("\n" + "# elements: " + myList.size() + "\n");
-
-        myList.entrySet().stream()
-                .forEach(System.out::println);
 
     }
-    }
+}
 
 
 
