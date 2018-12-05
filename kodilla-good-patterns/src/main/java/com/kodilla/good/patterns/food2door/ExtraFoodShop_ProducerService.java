e773d113060b1559bf22f379e0b7dabbdef4a49b
Marrucha <mmatczuk@g.pl>
2018-12-05 23:35:56 +0100

    9.4 Food suppliers v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ExtraFoodShop_ProducerService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ExtraFoodShop_ProducerService.java
@@ -4,9 +4,6 @@ public class ExtraFoodShop_ProducerService implements ProducerService {
 
     @Override
     public boolean process(Order order) {
-        if(order.getSupplier().isContract() && order.getQuantity() >= 100 && order.getOrderTime().getHour()<8 && order.getArticle().getArticleType().equals("Vegetables"))
-        return true;
-        else
-        return false;
+        return order.getSupplier().isContract() && order.getQuantity() >= 100 && order.getOrderTime().getHour()<8 && order.getArticle().getArticleType().equals("Vegetables");
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/HealthyShop_ProducerService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/HealthyShop_ProducerService.java
@@ -4,9 +4,6 @@ public class HealthyShop_ProducerService implements ProducerService{
 
     @Override
     public boolean process(Order order) {
-        if(order.getSupplier().isContract() && order.getArticle().getArticleType().equals("BioSpices") && order.getQuantity()< 10000)
-        return true;
-        else
-        return false;
+       return order.getSupplier().isContract() && order.getArticle().getArticleType().equals("BioSpices") && order.getQuantity()< 10000;
     }
 }
