553fff583c2a7489c0e1e444032c8dda996f275c
Marrucha <mmatczuk@g.pl>
2018-12-09 23:41:21 +0100

    10.3 ERROR?

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -8,18 +7,13 @@ import java.util.*;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
+    private LibraryDbController libraryDbController;
 
-    @Autowired
-    LibraryDbController libraryDbController;
-
-
-/*    @Autowired
-    public void setLibraryDbController(final LibraryDbController libraryDbController) {
+    public Library(final LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
-    }*/
+    }
 
     public Library() {
-        // do nothing
     }
 
     public void saveToDb() {
