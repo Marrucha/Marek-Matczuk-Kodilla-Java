970ded6fb167510fa87fd19c9f01dfb468653399
Marrucha <mmatczuk@g.pl>
2018-12-09 23:05:21 +0100

    10.2 Calculator iniection

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatortestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatortestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.spring.calculator;
 
 import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatortestSuite {
+    @Test
     public void checkCalculatorOperations() {
         //Given
         ApplicationContext context =
