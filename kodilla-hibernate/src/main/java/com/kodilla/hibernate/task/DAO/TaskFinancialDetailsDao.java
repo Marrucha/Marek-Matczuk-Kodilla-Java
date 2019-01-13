5d350c5b67e9051e426e8766f1974479e6449b3c
Marrucha <mmatczuk@g.pl>
2019-01-13 15:45:06 +0100

    13.3 Entity 1:N

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/DAO/TaskFinancialDetailsDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/DAO/TaskFinancialDetailsDao.java
@@ -1,4 +1,12 @@
 package com.kodilla.hibernate.task.DAO;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.List;
 
-public interface TaskFinancialDetailsDao {
+@Transactional
+@Repository
+public interface TaskFinancialDetailsDao extends CrudRepository<TaskFinancialDetails, Integer> {
+    List<TaskFinancialDetails> findByPaid(boolean paid);
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,4 +1,6 @@
 package com.kodilla.hibernate.task;
+import com.kodilla.hibernate.tasklist.TaskList;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.*;
@@ -10,6 +12,18 @@ public class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
+
+    @ManyToOne
+    @JoinColumn (name = "TASKLIST_ID")
+    public TaskList getTasklist() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList tasklist) {
+        this.taskList = tasklist;
+    }
 
     public Task(String description, int duration) {
         this.description = description;
@@ -19,6 +33,18 @@ public class Task {
 
     public Task(){
     };
+
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+
+    @OneToOne (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn (name = "TASK_FINANCIALS_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
 
     private void setId(int id) {
         this.id = id;
