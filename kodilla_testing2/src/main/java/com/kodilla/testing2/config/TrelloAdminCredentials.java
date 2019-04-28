28b1df2ac531fd60a279aaf5a0d6e85dbcf8de82
Marrucha <mmatczuk@g.pl>
2019-04-28 15:51:26 +0200

    23.4 Test

---
+++ kodilla_testing2/src/main/java/com/kodilla/testing2/config/TrelloAdminCredentials.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing2.config;
+
+public class TrelloAdminCredentials {
+    private static String login = "marekmatczuk";
+    private static String password = "Argentyna1";
+
+    public static String getLogin() {
+        return login;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
